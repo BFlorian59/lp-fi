b3468104fea3410ffc73c2dbdd15ddabfcd41872
BFlorian59 <77059018+BFlorian59@users.noreply.github.com>
2021-02-15 14:21:08 +0100

    afficher

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AppModule } from '../app.module';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    
+    
+  ],
+  exports:[
+    HeaderComponent, 
+    FooterComponent
+
   ]
 })
 export class CoreModule { }
