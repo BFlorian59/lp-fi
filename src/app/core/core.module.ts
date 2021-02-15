e44148b3851a3f538e472a19252be45a75b4bf4f
BFlorian59 <77059018+BFlorian59@users.noreply.github.com>
2021-02-15 14:49:02 +0100

    toolnav

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,14 +4,14 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AppModule } from '../app.module';
 import { AppRoutingModule } from '../app-routing.module';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule,
-    
+    MatToolbarModule
     
   ],
   exports:[
