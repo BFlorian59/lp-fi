837900ba16b8835eb210c80e99a5d99bd568565b
BFlorian59 <77059018+BFlorian59@users.noreply.github.com>
2021-02-15 16:15:19 +0100

    routing module

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from '../shared/components/not-found/not-found.component';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
-  
+  {
+    path:'',
+    component: HomeComponent
+  },
+
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
 ];
 
 @NgModule({
